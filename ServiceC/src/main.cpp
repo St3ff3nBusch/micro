c4e96bd19ee8e9b9a83bd7dfd089f3e407266698
Steffen Busch <steffen.busch@ikg.uni-hannover.de>
2025-09-09 16:02:38 +0200

    add test D response with by C

--- ServiceC/src/main.cpp
+++ ServiceC/src/main.cpp
@@ -22,12 +22,16 @@ public:
 
         Response reply;
         ClientContext context;
-
+    std::chrono::system_clock::time_point deadline =
+            std::chrono::system_clock::now() + std::chrono::seconds(2);
+        context.set_deadline(deadline);
         Status status = stub_->GetMessage(&context, request, &reply);
 
         if (status.ok()) {
             return reply.message();
-        } else {
+        }else if (status.error_code() == grpc::StatusCode::DEADLINE_EXCEEDED) {
+            return "RPC timeout";
+        }  else {
             return "RPC failed";
         }
     }
@@ -37,9 +41,19 @@ private:
 };
 
 int main() {
-    std::cout<<"ServiceC started"<<std::endl;
+      try {
     MyServiceClient client(grpc::CreateChannel("localhost:50051", grpc::InsecureChannelCredentials()));
-    std::string response = client.GetMessage("World");
-    std::cout << "ServiceC received: " << response << std::endl;
-    return 0;
+    std::string response = client.GetMessage("World2");
+
+      if (response.empty()) {
+            std::cerr << "ERROR: Response is empty!" << std::endl;
+            return 1;
+            }else {
+            std::cout << "ServiceC received test: " << response <<"return "<< !response.compare("Helloooo World")<<std::endl;
+                return !response.compare("Helloooo World");
+        }
+     } catch (const std::exception& e) {
+        std::cerr << "ServiceC error: " << e.what() << std::endl;
+        return 1;
+     }
 }
